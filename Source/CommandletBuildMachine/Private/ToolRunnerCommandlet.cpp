c5da416dd335bde691c4e7df812b045c69b5f1ac
Media CBL <musiquecbl@gmail.com>
2026-02-07 16:17:28 -0500

    Initial commit of Commandlet Build Machine

---
+++ Source/CommandletBuildMachine/Private/ToolRunnerCommandlet.cpp
@@ -0,0 +1,49 @@
+﻿// © Felix Courteau-Boutin All Right Reserved
+
+#include "ToolRunnerCommandlet.h"
+
+DEFINE_LOG_CATEGORY(LogToolRunnerCommandlet);
+
+int32 UToolRunnerCommandlet::Main(const FString& Params)
+{
+	UE_LOG(LogToolRunnerCommandlet, Display, TEXT("Tool Runner Commandlet Started"));
+	UE_LOG(LogToolRunnerCommandlet, Display, TEXT("Params: %s"), *Params);
+
+	// Parse arguments
+	FString Mode;
+	FString Target;
+
+	FParse::Value(*Params, TEXT("mode="), Mode);
+	FParse::Value(*Params, TEXT("target="), Target);
+
+	UE_LOG(LogToolRunnerCommandlet, Display, TEXT("Mode: %s"), *Mode);
+	UE_LOG(LogToolRunnerCommandlet, Display, TEXT("Target: %s"), *Target);
+
+	if (Mode.IsEmpty())
+	{
+		UE_LOG(LogToolRunnerCommandlet, Log, TEXT("Missing -mode argument"));
+		return 1;
+	}
+
+	// Call your logic
+	if (Mode == TEXT("build"))
+	{
+		UE_LOG(LogToolRunnerCommandlet, Log, TEXT("Run code build"));
+		//RunBuild(Target);
+	}
+	else if (Mode == TEXT("cook"))
+	{
+		UE_LOG(LogToolRunnerCommandlet, Log, TEXT("Run cook"));
+		//RunCook(Target);
+	}
+	else
+	{
+		UE_LOG(LogToolRunnerCommandlet, Error, TEXT("Unknown mode"));
+		return 1;
+	}
+
+	UE_LOG(LogToolRunnerCommandlet, Display, TEXT("Finished Successfully"));
+
+	return 0;
+}
+
---
+++ Source/CommandletBuildMachine/Public/ToolRunnerCommandlet.h
@@ -0,0 +1,32 @@
+﻿// © Felix Courteau-Boutin All Right Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Commandlets/Commandlet.h"
+#include "ToolRunnerCommandlet.generated.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogToolRunnerCommandlet, Log, All);
+
+/**
+ * 
+ */
+UCLASS(CustomConstructor)
+class COMMANDLETBUILDMACHINE_API UToolRunnerCommandlet : public UCommandlet
+{
+	GENERATED_UCLASS_BODY()
+	
+	UToolRunnerCommandlet(
+	const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+	{
+		IsClient = false;
+		IsEditor = false;
+		IsServer = false;
+
+		LogToConsole = true;
+		ShowErrorCount = true;
+	}
+
+	virtual int32 Main(const FString& Params) override;
+};
